e132e7096392404942f521179795a97b1061764a
Arnab Dev <official.arnabdev@gmail.com>
2023-01-23 16:09:30 +0600

    Update vectors in c++.cpp

--- vectors in c++.cpp
+++ vectors in c++.cpp
@@ -1,131 +1,125 @@
-#include <bits/stdc++.h>
-using namespace std;
+# Maps Cheat Sheet
 
+## Header
+```c++
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define nl cout<<endl;
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define input(a,n) for(int i=0; i<n; i++){ cin>>a[i]; }
+#define print(a,n) for(int i=0; i<n; i++){ cout<<a[i]<<" , "; }
+#define loop(a,b) for(int i=a;i<b;i++)
+```
+/// vector_name.size() gives the size of the vector
+## Creating, adding elements and copying a Vector
+```c++
+void (){
+   ///vector <type> name (elements);
+   ///vector of size 5 with no elements
+   vector<int> data (5); /// equivalent of this in an array is int data[5];
+   ///vector of size 5 with all elements equal to 2    
+   vector<int> data1(5, 2);
+   ///vector of specified elements    
+   vector<int> data2({1, 2, 3, 4, 5}); /// equivalent of this in an array is int data[] = {1, 2, 3, 4, 5}
+   vector<int> data3 = {1, 2, 3, 4, 5}; /// equivalent of this in an array is int data[] = {1, 2, 3, 4, 5}
+   ///copying a vector
+   vector<int> data4 = data;
+}
+```
+## Printing the Vector
+```c++
 void print(vector<int> data){
     cout << "Total Elements: " << data.size() << "\n";
-    for (int datam:data)
-        cout << datam << " ";
+    for (int it:data)
+        cout << it << " ";
     cout << "\n\n";
 }
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    /// creating a vector
-    ///vector <type> name (elements);
-    
-    ///vector of size 5 with no elements
-//    vector<int> data (5); /// int data[5];
-//    print(data);
-    
-
-    ///vector of size 5 with all elements equal to 2    
-//    vector<int> data(5, 2);
-//    print(data);
-
-    ///vector of specified elements and copying a vector    
-//    vector<int> data({1, 2, 3, 4, 5}); /// int data[] = {1, 2, 3, 4, 5}
-//    vector<int> data = {1, 2, 3, 4, 5}; /// int data[] = {1, 2, 3, 4, 5}
-//    print(data);
-//    vector<int> data2 = data;
-//    print(data2);
-
-    
-    /// adding elements
-//    vector <int> data = {1,2,3};
-//    data.push_back(5);
-//    data.push_back(7);
-//    data.push_back(9);
-//    print(data);
+```
 
 
+```
+## Inserting elements, updating elements, indices and pointing to elements(.begin(), .end(), .insert())
+```c++
+void solve(){
+   /// adding elements
+   vector <int> data = {1,2,3};
+   data.push_back(5);
+   data.push_back(7);
+   data.push_back(9);
+   /// data.begin() points to the first element
+   /// data.insert(a, b) adds element b to position a
+   /// data.insert(a, c, b) adds element b to position a, c times
+   data.insert( data.begin() , 7);
+   data.insert( data.begin() + 2, 7);
+   /// data.end() points to the last element
+   data.insert(data.end()-1, 7);
+   data.insert(data.begin(), 2, 7);
+   data.insert(data.begin() + 1, 2, 7); 
+    
+    
+   /// updating values and accessing values using index
+   vector <int> data1 = {3, 2, 1, 6, 4, 2};
+   data1[2] = 7;
+   cout << data1[2] << "\n";
+}
+```
 
-    /// data.begin() points to the first element
-    /// data.end() points to the last element
-    /// data.insert(a, b) adds element b to position a
-    /// data.insert(a, c, b) adds element b to position a, c times
-//    vector <int> data = {1,2,3};
-//    data.insert( data.begin() , 7);
-//    data.insert( data.begin() + 2, 7);
-//    data.insert(data.end()-1, 7);
-//    data.insert(data.begin(), 2, 7);
-//    data.insert(data.begin() + 1, 2, 7);
-//    print(data);
-    
-    
-    
-     /// sorting a vector
-//     vector <int> v = {1,2,3};
-//     sort(v.begin(), v.end());
-//     sort(v.begin(), v.end(), greater<int>());
+```
+## Sorting a Vector
+```c++
+void solve(){
+    vector <int> v = {1,2,3};
+    sort(v.begin(), v.end());  ///Ascending
+    sort(v.begin(), v.end(), greater<int>()); ///Descending  
+}
+```
 
-    
-    
-    /// reading data from vector
-    /// vector_name.size() gives the size of the vector
-//    vector <int> data = {3, 2, 1, 6, 4, 2};
-//    cout << data[2] << "\n";
-//    cout << data.front() << "\n";
-//    cout << data.back() << "\n";
-//    vector <int> data = {3, 2, 1, 6, 4, 2};
-//    for (int d:data){
-//        cout << d << " ";
-//    }
-//    vector <int> data = {3, 2, 1, 6, 4, 2};
-//    int ln = data.size();
-//    for (int i=0; i<ln; ++i){
-//        cout << data[i] << " ";
-//    }
-//    vector <int> data = {3, 2, 1, 6, 4, 2};
-//    int ln = data.size();
-//    for (int i=2; i<ln-1; ++i){
-//        cout << data[i] << " ";
-//    }
-//    vector <int> data = {3, 2, 1, 6, 4, 2};
-//
-//    int *d = data.data();
-//    cout << d[2];
-
-    
-    
-    /// updating values
-//    vector <int> data = {3, 2, 1, 6, 4, 2};
-//    data[2] = 7;
-//    cout << data[2] << "\n";
-//    print(data);
-
-    
-    
-    
+```
+## Swapping, clearing and, erasing
+```c++
+void solve(){
     /// swapping values using .swap()
-//    vector <int> data = {3, 2, 1, 6, 4, 2};
-//    vector <int> data2 = {6, 9, 8};
-//
-//    cout << "before swap\n";
-//    print(data);
-//    print(data2);
-//
-//    data.swap(data2);
-//    cout << "after swap\n";
-//    print(data);
-//    print(data2);
-
-
+   vector <int> data = {3, 2, 1, 6, 4, 2};
+   vector <int> data2 = {6, 9, 8};
+   data.swap(data2);   /// all values get swapped
     
-    
-    /// deleting elements
-//    vector <int> data = {3, 2, 1, 6, 4, 2};
-//    cout << "Before delete\n";
-//    print(data);
-//    data.pop_back();
-//    data.clear();
-//    data.erase(data.begin()+2, data.begin()+3);
-//    data.erase(data.begin()+2, data.end()-2);
-//    cout << "After delete\n";
-//    print(data);
+   /// deleting elements
+   data.pop_back();   /// remove an element from the back
+   data.clear();      ///  used to remove all the elements
+   
+   /// vector_name.erase(position);    for deletion at specific position
+   /// vector_name.erase(starting_position, ending_position);   for deletion in range
+   data.erase(data.begin()+2, data.begin()+3); /// deletes elements number 3 to 4
+   data.erase(data.begin()+2, data.end()-2);   /// deletes element number 3 to 3rd last(last 2 elements stay)
+}
+```
+```
+## Different ways of reading from a vector
+```c++
+void solve(){
+   vector <int> data = {3, 2, 1, 6, 4, 2};
+   cout << data[2] << "\n";
+   cout << data.front() << "\n";   /// first element
+   cout << data.back() << "\n";    /// last element
+   /// printing every element 
+   for (int d:data){
+       cout << d << " ";
+   }
+   /// Or
+   int ln = data.size();
+   for (int i=0; i<ln; ++i){
+       cout << data[i] << " ";
+   }    
+}
+```
 
 
+## Main
+```c++
+int main(){
+    solve();
     return 0;
 }
+```
